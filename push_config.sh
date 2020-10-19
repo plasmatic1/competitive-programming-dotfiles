3e1a13bad660d9f2fc8b1ea415a2134f8d1d1c5a
Plasmatic <moses@mosesxu.net>
2020-10-18 22:43:17 -0400

    push_config now makes symlink to templates

--- push_config.sh
+++ push_config.sh
@@ -1,10 +1,14 @@
 #!bash
 
-judge=(dmoj cf sgu fhc unfinished)
+judges=(dmoj cf sgu fhc unfinished)
+WINHOME=$(cmd.exe /C "cd /D %USERPROFILE% && bash.exe -c pwd")
 
-for d in ${judge[@]}; do
+for d in ${judges[@]}; do
     cp .template.cpp $d/.template.cpp
     cp -r .cptools $d/.cptools
+
+    rm -f $d/templates
+    ln -s $WINHOME/Dropbox/templates $d/templates
+
     echo "Done $d/"
 done
-
